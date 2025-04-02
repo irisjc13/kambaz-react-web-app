fb3f4c1f8dcaebe193cc7036cdd41993b21ab5c9
Iris Carrillo <carrillo.iris2020@gmail.com>
2025-04-02 11:26:30 -0400

    i deleted a line in Kambaz/index.tsx

--- src/Kambaz/index.tsx
+++ src/Kambaz/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Routes, Route, Navigate } from "react-router";
 import Account from "./Account";
